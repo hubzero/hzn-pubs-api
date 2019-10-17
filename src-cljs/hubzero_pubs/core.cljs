eb178a62c5df955d3ec1c52eca11c02f71834d4c
JBG <jbg@peerparty.org>
2019-10-17 14:30:31 +0200

    componentized version of ilya's stuff

--- src-cljs/hubzero_pubs/core.cljs
+++ src-cljs/hubzero_pubs/core.cljs
@@ -62,11 +62,16 @@
    :state (fn [] {}) }) 
 
 (g/defcomponent
+  :gallery-image
+  {:template "#gallery-image"
+   :props [:name]
+   :state (fn [] {}) }) 
+
+(g/defcomponent
   :files
   {:template "#collection"
    :props [:title]
    :data (fn [] { :files ["first-file.pdf" "second-file.pdf" "third-file.pdf"] }) 
- 
    :state (fn [] {}) })
 
 (g/defcomponent
@@ -79,6 +84,20 @@
                             ] }) 
    :state (fn [] {}) }) 
 
+(g/defcomponent
+  :image-gallery
+  {:template "#collection"
+   :props [:title]
+   :data (fn [] { :images ["sd-first-visit.png" "sd-first-visit.png" "sd-first-visit.png"] }) 
+   :state (fn [] {}) })
+
+(g/defcomponent
+  :support-docs
+  {:template "#collection"
+   :props [:title]
+   :data (fn [] {:files  ["first-file.pdf" "second-file.pdf" "third-file.pdf"]
+                 :images ["sd-first-visit.png" "sd-first-visit.png" "sd-first-visit.png"] }) 
+   :state (fn [] {}) })
 
 (g/defcomponent
   :icon
@@ -87,12 +106,53 @@
    :state (fn [] {}) }) 
 
 (g/defcomponent
+  :tag
+  {:template "#tag"
+   :props [:name]
+   :state (fn [] {}) }) 
+
+(g/defcomponent
+  :tag-creator
+  {:template "#tag-creator"
+   :state (fn [] {}) }) 
+
+(g/defcomponent
+  :tags
+  {:template "#tags"
+   :data (fn [] { :tags ["Publication" "Publicate"] })
+   :state (fn [] {}) })
+
+(g/defcomponent
+  :citations
+  {:template "#collection"
+   :props [:title]
+   :data (fn [] { :files ["Paskin, N. (1999). Toward unique identifiers. Proceedings of the IEEE, 87(7), 1208–1227. doi:10.1109/5.771073"
+                          "Paskin, N. (1999). Toward unique identifiers. Proceedings of the IEEE, 87(7), 1208–1227. doi:10.1109/5.771073"] }) 
+   :state (fn [] {}) })
+
+(g/defcomponent
+  :agreements
+  {:template "#agreements" :state (fn [] {}) }) 
+
+(g/defcomponent
   :essentials 
   {:template "#essentials" :state (fn [] {}) }) 
 
 (g/defcomponent
   :additional-details 
   {:template "#additional-details" :state (fn [] {}) }) 
+
+(g/defcomponent
+  :publish-settings 
+  {:template "#publish-settings" :state (fn [] {}) }) 
+
+(g/defcomponent
+  :section-buttons 
+  {:template "#section-buttons" :state (fn [] {}) }) 
+
+(g/defcomponent
+  :aside-buttons 
+  {:template "#aside-buttons" :state (fn [] {}) }) 
 
 (g/defcomponent
   :license
