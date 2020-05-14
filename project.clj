0a6933f57aa5a8ff542edea47888597ae65ef2cf
JBG <jbg@peerparty.org>
2020-05-14 12:14:20 +0200

    okay this is working again

--- project.clj
+++ project.clj
@@ -1,8 +1,38 @@
-(defproject hubzero-pubs "0.1.0-SNAPSHOT"
+(defproject hzn-pubs-api "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+;  :dependencies [[org.clojure/clojure "1.10.0"]
+;                 [org.clojure/tools.logging "0.4.1"]
+;                 [org.clojure/tools.nrepl "0.2.13"]
+;                 [org.clojure/tools.cli "0.3.7"]
+;                 [org.clojure/java.jdbc "0.7.11"]
+;                 [cprop "0.1.15"]
+;                 [mount "0.1.12"]
+;                 [org.immutant/immutant "2.1.10"]
+;                 [metosin/jsonista "0.2.6"]
+;                 [compojure "1.6.1"]
+;                 [com.cognitect/transit-clj "0.8.309"]
+;                 [markdown-clj "1.0.2"]
+;                 [ring-webjars "0.2.0"]
+;                 [ring/ring-core "1.8.1"]
+;                 [ring/ring-defaults "0.3.2"]
+;                 [metosin/muuntaja "0.6.7"]
+;                 [metosin/ring-http-response "0.9.1"]
+;                 [clj-http "3.10.1"]
+;                 [mysql/mysql-connector-java "8.0.20"]
+;                 [yesql "0.5.3"]
+;                 [clj-time "0.15.2"]
+;                 [digest "1.4.9"]
+;                 [me.raynes/fs "1.4.6"]
+;                 [com.novemberain/monger "3.1.0"]
+;
+;                 ;; hzn - JBG
+;                 [hzn-session-auth "0.1.0-SNAPSHOT"]
+;                 ]
+
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.1"]
@@ -32,7 +62,7 @@
 
 :profiles {:uberjar {:aot :all :omit-source true}}
 
-:main ^:skip-aot hubzero-pubs.core
+:main ^:skip-aot hzn-pubs-api.core
 :target-path "target/%s"
 )
 
